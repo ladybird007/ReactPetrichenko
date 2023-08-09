2ebdd82efa8fea5e56cb252be3ce135a90ecdec0
ishunter216 <michael.king@edenprogram.com>
2023-08-09 11:05:40 +0300

    create app

---
+++ my-app/src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./header.css";
+
+const Header = () => {
+    return (
+        <div className="header d-flex">
+            <h1>Anastasiia</h1>
+            <h2>5 posts, liked 0</h2>
+        </div>
+    )
+}
+
+export default Header;
